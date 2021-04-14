82824550cd201beb2765e165d803ada509002498
jamessamantha099 <jamessamantha099@gmail.com>
2021-04-14 11:04:37 -0500

    Added initial starter files

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect 
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config.from_object(Config)
+csrf = CSRFProtect(app)
+
+from app import views,models
